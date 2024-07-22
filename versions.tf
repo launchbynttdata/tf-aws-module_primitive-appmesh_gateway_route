96978afab343a0195beca7a01823e392757a1cb5
Chris Day <Chris.Day@nttdata.com>
2024-07-22 17:37:33 -0400

    Re-adds provider configs in root versions.tf while allowing for submodule provider.tf files

--- versions.tf
+++ versions.tf
@@ -20,3 +20,13 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region  = "us-east-2"
+  profile = "default"
+}
+
+provider "aws" {
+  alias   = "global"
+  region  = "us-east-1"
+  profile = "default"
+}
